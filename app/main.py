d490a7201d0c3e5eb505c739974a259d89a61a48
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-09-28 12:03:51 +0200

    solved py-linter-formatter

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
+            format_linter_error(error) for error in errors
         ], "path": file_path, "status": "passed"
         if len(errors) == 0 else "failed"
     }
@@ -25,15 +19,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {"line": error["line_number"],
-                 "column": error["column_number"],
-                 "message": error["text"],
-                 "name": error["code"],
-                 "source": "flake8"}
-                if len(error) != 0 else [] for error in errors
-            ], "path": file_path,
-            "status": "passed" if len(errors) == 0 else "failed"
-        } for file_path, errors in linter_report.items()
+        format_single_linter_file(file_path=file_path, errors=errors)
+        for file_path, errors in linter_report.items()
     ]
